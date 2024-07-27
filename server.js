632fe82043816ee88a107e2075ffc907e9e84f0d
Basantsaeed <89366449+Basantsaed@users.noreply.github.com>
2024-07-27 10:53:39 +0300

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const path = require('path');
+const path = require('path'); // Ensure path is required to handle file paths
+const login = require('./src/component/LoginAdmin/Login');
 
 const app = express();
 
-// Serve static files from the React app
+//Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.use('/login', login);
+
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
@@ -16,3 +20,4 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
